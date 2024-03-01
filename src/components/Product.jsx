c0844f8ec8d4d1071a3714b2a61ab31036a9326f
spanzy teo <spanzytheo@gmail.com>
2024-03-01 16:38:52 +0100

    changes

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import { trendingItems } from '../utils/trendingItems'
+import { useParams } from 'react-router-dom'
 
-const Product = () => {
-  return <div>Product</div>
+const Product = ({ match }) => {
+  const { id } = useParams()
+
+  const itemId = parseInt(id)
+  const item = trendingItems.find((item) => item.id === itemId)
+  return (
+    <Box
+      flexDirection={{ xs: 'column', lg: 'row' }}
+      alignItems="center"
+      justifyContent="space-between"
+      sx={{
+        mt: { xs: '0px', lg: '50px' },
+        mx: { xs: '0rem', lg: '2rem' },
+      }}
+    ></Box>
+  )
 }
 
 export default Product
